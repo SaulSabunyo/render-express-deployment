0c960eb41bfafda72b18a2d1c4609fb607209381
SaulWeaponz <saulweaponz@gmail.com>
2025-10-19 16:22:53 +0300

    Add the files

---
+++ server.js
@@ -0,0 +1,11 @@
+const require = require('express');
+const app = express();// Initialize Express application
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Thanks for connecting to my Channel!')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
